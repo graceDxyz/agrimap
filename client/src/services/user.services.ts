8bddb3a9ba046995cb8ad7a8e5a6c8d6ea8de0a5
Brixter Porras <brixterporras@gmail.com>
2023-09-12 22:08:58 +0800

    random commits

--- client/src/services/user.services.ts
+++ client/src/services/user.services.ts
@@ -2,7 +2,7 @@ import { QUERY_USERS_KEY } from "@/constant/query.constant";
 import { SERVER } from "@/constant/server.constant";
 import { User } from "@/types/user.type";
 import { useQuery } from "@tanstack/react-query";
-import { request, gql } from "graphql-request";
+import { gql, request } from "graphql-request";
 interface UsersQueryResponse {
   users: User[];
 }
@@ -11,6 +11,10 @@ export function useGetUsers() {
   return useQuery({
     queryKey: [QUERY_USERS_KEY],
     queryFn: async () => {
+      const headers = {
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NGZmMzY2MDY3YWUzODBmY2NmM2FkMDkiLCJpYXQiOjE2OTQ0OTA2MTMsImV4cCI6MTY5NDQ5NDIxM30.qq-T7_RWyLK-W1PInEYJRrQ5cXwEgZsDOxVzoS9gNkk`, // Set the Authorization header with the token
+      };
+
       const data = await request<UsersQueryResponse>(
         SERVER,
         gql`
@@ -26,6 +30,7 @@ export function useGetUsers() {
             }
           }
         `,
+        { headers }
       );
       return data.users;
     },
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,18 +1,27 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
+import * as morgan from 'morgan';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Use the MorganMiddleware to log requests and responses
+  app.use(morgan('common'));
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://http://192.168.254.180/:5173'],
+    origin: ['*'],
     credentials: true,
-    allowedHeaders:
-      'Content-Type,Accept,Authorization,Access-Control-Allow-Origin',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Include 'OPTIONS' for preflight requests
+    allowedHeaders: [
+      'Content-Type',
+      'Accept',
+      'Authorization',
+      'Access-Control-Allow-Origin',
+    ],
   });
 
   app.use(
@@ -20,12 +29,21 @@ async function bootstrap() {
       contentSecurityPolicy: {
         directives: {
           scriptSrc: ["'self'", "'unsafe-inline'", 'https://cdn.jsdelivr.net'],
-          imgSrc: ["'self'", 'data:', 'https://cdn.jsdelivr.net'],
+          imgSrc: [
+            "'self'",
+            'data:',
+            'https://cdn.jsdelivr.net',
+            'http://cdn.jsdelivr.net',
+          ], // Add 'http://cdn.jsdelivr.net' to img-src
         },
       },
     }),
   );
 
-  await app.listen(5000);
+  // Use the ConfigService to access the port configuration
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 5000); // Use a default value (e.g., 5000)
+
+  await app.listen(port);
 }
 bootstrap();
