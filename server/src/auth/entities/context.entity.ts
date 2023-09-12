3974938a7d47443b4ac47651d1c2c77a273838d0
Brixter Porras <brixterporras@gmail.com>
2023-09-12 23:00:04 +0800

    refactor: to express api

--- server/src/auth/entities/context.entity.ts
+++
@@ -1,7 +0,0 @@
-import { ExecutionContext } from '@nestjs/common';
-import { User } from 'src/users/entities/user.entity';
-
-export interface IGqlContext extends ExecutionContext {
-  user?: User; // Add your custom property here, such as the user object
-  // Add other custom properties if needed
-}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,49 +1,19 @@
-import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
-import helmet from 'helmet';
-import * as morgan from 'morgan';
-import { AppModule } from './app.module';
+import express from "express";
+import dotenv from "dotenv";
+import config from "config";
+import connect from "@/utils/connect";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+dotenv.config();
+const port = config.get<number>("port");
 
-  // Use the MorganMiddleware to log requests and responses
-  app.use(morgan('common'));
-  app.useGlobalPipes(new ValidationPipe());
+const app = express();
 
-  app.enableCors({
-    origin: ['*'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Include 'OPTIONS' for preflight requests
-    allowedHeaders: [
-      'Content-Type',
-      'Accept',
-      'Authorization',
-      'Access-Control-Allow-Origin',
-    ],
-  });
+app.use(express.json());
+app.get("/healthcheck", (req, res) => {
+  res.send({ message: "☁☁☁" });
+});
 
-  app.use(
-    helmet({
-      contentSecurityPolicy: {
-        directives: {
-          scriptSrc: ["'self'", "'unsafe-inline'", 'https://cdn.jsdelivr.net'],
-          imgSrc: [
-            "'self'",
-            'data:',
-            'https://cdn.jsdelivr.net',
-            'http://cdn.jsdelivr.net',
-          ], // Add 'http://cdn.jsdelivr.net' to img-src
-        },
-      },
-    }),
-  );
-
-  // Use the ConfigService to access the port configuration
-  const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT', 5000); // Use a default value (e.g., 5000)
-
-  await app.listen(port);
-}
-bootstrap();
+app.listen(port, async () => {
+  console.log(`App is running at http://localhost:${port}`);
+  await connect();
+});
