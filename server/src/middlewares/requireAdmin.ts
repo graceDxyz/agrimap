d690c4dbc0b034ed44adba9bec79122aefd5873b
Brixter Porras <brixterporras@gmail.com>
2023-09-14 18:13:37 +0800

    feat: Added requried admin user

---
+++ server/src/middlewares/requireAdmin.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+
+const requiredAdmin = (req: Request, res: Response, next: NextFunction) => {
+  const user = res.locals.user;
+
+  if (!user || user?.userRole != "ADMIN") {
+    return res.sendStatus(403);
+  }
+
+  return next();
+};
+
+export default requiredAdmin;
--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,8 +1,9 @@
 import { getAllUserHandler } from "@/controllers/user.controller";
+import requiredAdmin from "@/middlewares/requireAdmin";
 import { Express } from "express";
 
 function UserRoutes(app: Express) {
-  app.get("/api/users", getAllUserHandler);
+  app.get("/api/users", requiredAdmin, getAllUserHandler);
 }
 
 export default UserRoutes;
