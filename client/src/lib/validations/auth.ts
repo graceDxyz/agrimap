35a00a13562bacff139b6f997f8d453cc983231b
Brixter Porras <brixterporras@gmail.com>
2023-09-15 16:37:37 +0800

    refactor: react-hook-form to old version

--- client/src/lib/validations/auth.ts
+++ client/src/lib/validations/auth.ts
@@ -1,11 +1,15 @@
 import * as z from "zod";
 
 export const authSchema = z.object({
-  email: z.string().email({
-    message: "Please enter a valid email address",
-  }),
+  email: z
+    .string()
+    .nonempty({ message: "Please enter a valid email address" })
+    .email({
+      message: "Please enter a valid email address",
+    }),
   password: z
     .string()
+    .nonempty({ message: "Password must be at least 8 characters long" })
     .min(8, {
       message: "Password must be at least 8 characters long",
     })
