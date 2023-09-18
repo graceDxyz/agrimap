6c2bda44bcb42e7055033237ec51f07cc8335780
Brixter Porras <brixterporras@gmail.com>
2023-09-18 22:43:38 +0800

    style: sidebar to fixtop

--- client/src/components/shells/layout-shell.tsx
+++ client/src/components/shells/layout-shell.tsx
@@ -9,7 +9,7 @@ export function DashboardShell() {
   return (
     <div className="flex min-h-screen flex-col">
       <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-5">
-        <aside className="fixed  z-30 -ml-2 hidden h-screen w-full shrink-0 overflow-y-auto border-r md:sticky md:block">
+        <aside className="fixed top-0 z-30 -ml-2 hidden h-screen w-full shrink-0 overflow-y-auto border-r md:sticky md:block">
           <ScrollArea className="py-6 pr-6 lg:py-8">
             <div className="h-20 flex">
               <Link
