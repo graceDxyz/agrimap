e12639dd9f9e214be17d3080b228218782192f6e
Brixter Porras <brixterporras@gmail.com>
2023-09-18 22:51:25 +0800

    feat: added statistic report ui page

--- client/src/pages/ReportsPage.tsx
+++ client/src/pages/ReportsPage.tsx
@@ -1,5 +1,34 @@
+import {
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from "@/components/page-header";
+import { Shell } from "@/components/shells/shell";
+import { Button } from "@/components/ui/button";
+
 function ReportsPage() {
-  return <div>ReportsPage</div>;
+  return (
+    <Shell variant="sidebar">
+      <PageHeader
+        id="dashboard-stores-page-header"
+        aria-labelledby="dashboard-stores-page-header-heading"
+      >
+        <div className="flex space-x-4">
+          <PageHeaderHeading size="sm" className="flex-1">
+            Statistic Report
+          </PageHeaderHeading>
+          <Button size="sm">Add report</Button>
+        </div>
+        <PageHeaderDescription size="sm">
+          Manage the statistic reports
+        </PageHeaderDescription>
+      </PageHeader>
+      <section
+        id="dashboard-stores-page-stores"
+        aria-labelledby="dashboard-stores-page-stores-heading"
+      ></section>
+    </Shell>
+  );
 }
 
 export default ReportsPage;
