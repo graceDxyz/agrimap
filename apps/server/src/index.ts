7d32cfc5c2672690414ba99a654fa839b1a2e36b
Brix <brixterporras@gmail.com>
2023-11-10 12:18:01 +0800

    feat: added gracefull shutdown

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -10,8 +10,17 @@ import { seed } from "./utils/generated";
 
 const server = createServer();
 
-server.listen(env.PORT, async () => {
+const app = server.listen(env.PORT, async () => {
   await connect();
   await seed();
   logger.info(`API running on port:${env.PORT}`);
+});
+
+process.on("SIGTERM", () => {
+  logger.info("SIGTERM signal received.");
+  logger.info("Closing http server.");
+  app.close((err) => {
+    logger.info("Http server closed.");
+    process.exit(err ? 1 : 0);
+  });
 });
